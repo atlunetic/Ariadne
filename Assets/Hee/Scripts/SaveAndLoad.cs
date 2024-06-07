c30add07262b18b3b034611ca4a3a7dbd6323da8
Ssomewhat <isc10120@gmail.com>
2024-06-07 17:57:47 +0900

    저장기능

---
+++ Assets/Hee/Scripts/SaveAndLoad.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json;
+using System.IO;
+using Yarn.Unity;
+
+public class SaveAndLoad : MonoBehaviour
+{
+    private string FolderPath;
+    void Awake(){
+    #if UNITY_EDITOR
+        FolderPath = $"{Application.dataPath}/Saves/";
+    #else
+        FolderPath = $"{Application.persistentDataPath}/Saves/";
+    #endif
+    }
+    public void Save(){
+        if (Directory.Exists(FolderPath) == false){
+            Directory.CreateDirectory(FolderPath);
+        }
+
+        GameManager.instance.items = Inventory.instance.items;
+        string json = JsonConvert.SerializeObject(GameManager.instance, Formatting.Indented);
+
+        string Totalpath = FolderPath+"Save";
+        int i=0;
+        // while (File.Exists(Totalpath + i.ToString())) i++;
+        File.WriteAllText(Totalpath + i.ToString(), json);
+        Debug.Log(Totalpath + i.ToString()+" 에 저장완료");
+    }
+
+    public void Load(string filename){
+
+        DestroyImmediate(GameObject.Find("MenuUI"));
+        string json = File.ReadAllText(FolderPath+filename);
+
+        int num = GameManager.instance.NumOfScreenShots;
+
+        GameManager.instance = JsonConvert.DeserializeObject<GameManager>(json);
+        GameManager.instance.IsLoad = true;
+        GameManager.instance.NumOfScreenShots = num > GameManager.instance.NumOfScreenShots ? num : GameManager.instance.NumOfScreenShots;
+
+        SceneManager.LoadScene("Menu");
+
+        var runner = FindObjectOfType<DialogueRunner>();
+        runner.StartDialogue("RandomN");
+
+        Debug.Log("저장된 씬: "+GameManager.instance.NowScene);
+    }
+
+    public void LoadSave0(){
+        Load("Save0");
+    }
+}
