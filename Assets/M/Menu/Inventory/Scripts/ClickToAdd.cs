ec9dd95ac5a664764722e14c081e44510f1bd8aa
atlunetic <150178474+atlunetic@users.noreply.github.com>
2024-06-18 20:53:14 +0900

    Update ClickToAdd.cs

--- Assets/M/Menu/Inventory/Scripts/ClickToAdd.cs
+++ Assets/M/Menu/Inventory/Scripts/ClickToAdd.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class ClickToAdd : MonoBehaviour
 {
