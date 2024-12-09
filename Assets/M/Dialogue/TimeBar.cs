2ddff7d6d5e2f80ffe56cf3721b3ac3b6f03e9fa
atlunetic <150178474+atlunetic@users.noreply.github.com>
2024-12-10 02:12:26 +0900

    1210

--- Assets/M/Dialogue/TimeBar.cs
+++ Assets/M/Dialogue/TimeBar.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Yarn.Unity;
-
+using System.Collections;
 
 public class TimeBar : MonoBehaviour
 {
-    public GameObject timeSprites;
+    public GameObject timeSprites; 
     public Image timeBar;
     public float timeLimit;
     private float timeLeft;
     private string targetScene;
     private bool isPaused = true;
+    private DialogueRunner dialogueRunner; 
+    public OptionsListView optionsListView;
 
     [YarnCommand("ShowTB")]
     public void ShowTimeBar(bool status)
@@ -27,6 +29,17 @@ public class TimeBar : MonoBehaviour
         targetScene = sceneName;
         isPaused = true;
 
+        // Ensure the DialogueRunner reference is set
+        if (dialogueRunner == null)
+        {
+            dialogueRunner = FindObjectOfType<DialogueRunner>();
+        }
+
+        // Ensure the OptionsListView reference is set
+        if (optionsListView == null)
+        {
+            optionsListView = FindObjectOfType<OptionsListView>();
+        }
     }
 
     [YarnCommand("PauseTB")]
@@ -48,37 +61,61 @@ public class TimeBar : MonoBehaviour
         isPaused = true;
         Debug.Log("isPaused is currently " + isPaused);
     }
+
     void Update()
     {
         if (!isPaused)
         {
-            //Debug.Log("Timer Start");
-            //Debug.Log("isPaused is currently " + isPaused);
             if (timeLeft > 0)
             {
                 timeLeft -= Time.deltaTime;
                 timeBar.fillAmount = timeLeft / timeLimit;
             }
-
             else
             {
                 HandleTimeOut();
             }
         }
-        else
-        {
-            //Debug.Log("Blocked by isPaused");
-        }
     }
 
     private void HandleTimeOut()
     {
-        var runner = FindObjectOfType<DialogueRunner>();
-        runner.Stop();
+        dialogueRunner.Stop();
+        Debug.Log("Dialogue Stopped");
+        StartCoroutine(HideOptionsView());
+        Debug.Log("Options view manually hidden.");
+
+        var dialogueViews = dialogueRunner.dialogueViews;
+        foreach (var view in dialogueViews)
+        {
+            if (view is OptionsListView optionsView)
+            {
+                optionsView.DialogueComplete();
+                Debug.Log("OptionsListView DialogueComplete called.");
+            }
+        }
+
+
         SceneManager.LoadScene(targetScene);
     }
 
+    private IEnumerator HideOptionsView()
+    {
+        if (optionsListView != null)
+        {
+            var canvasGroup = optionsListView.GetComponent<CanvasGroup>();
+            if (canvasGroup != null)
+            {
+                yield return StartCoroutine(Effects.FadeAlpha(canvasGroup, canvasGroup.alpha, 0, 0.1f));
+            }
 
+            // Disable all option views
+            foreach (var optionView in optionsListView.GetComponentsInChildren<OptionView>())
+            {
+                optionView.gameObject.SetActive(false);
+            }
 
-
+            Debug.Log("Options view hidden.");
+        }
+    }
 }
